61f6cfa1f845d6aa1f6a3803f2e70b7c9c247886
k022g0004@g.neec.ac.jp <k022g0004@g.neec.ac.jp>
2023-07-14 12:46:50 +0900

    ImGuiの直し

--- main.cpp
+++ main.cpp
@@ -59,7 +59,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		ImGui::DragFloat3("CameraTranslate", &cameraTranslate.x, 0.01f);
 		ImGui::DragFloat3("CameraRotate", &cameraRotate.x, 0.01f);
 		ImGui::DragFloat3("SphereCenter", &sphere.center.x, 0.01f);
-		ImGui::DragFloat3("SphereRadius", &sphere.radius, 0.01f);
+		ImGui::DragFloat("SphereRadius", &sphere.radius, 0.01f);
 		ImGui::End();
 
 		///
